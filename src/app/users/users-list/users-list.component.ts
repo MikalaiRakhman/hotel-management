f1dc7319d9aad6b58a1e2f026d4a1e62e0b139ee
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-01-10 15:30:42 +0100

    Created users-edit-component. Added new func onEditUser(user: User) in users-list-component.

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -3,6 +3,8 @@ import { MatTableModule} from '@angular/material/table'
 import { User } from '../../models/user.type'
 import { UserService } from '../../services/user-service/user.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog'
+import { UsersEditComponent } from '../users-edit/users-edit.component';
 
 @Component({
   selector: 'app-users-list',
@@ -12,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class UsersListComponent implements OnInit {
   userService = inject(UserService);
+  dialog = inject(MatDialog)
 
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'phoneNumber', 'actions']
   users = signal<Array<User>>([]);
@@ -22,5 +25,18 @@ export class UsersListComponent implements OnInit {
 
   loadUsers(): void {
     this.userService.getUsers().subscribe((data: Array<User>) => {this.users.set(data)})
+  }
+
+  onEditUser(user: User): void {
+    const dialogRef = this.dialog.open(UsersEditComponent, {
+      data: user,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('User data after edit', result);
+        this.loadUsers();
+      }
+    })
   }
 }

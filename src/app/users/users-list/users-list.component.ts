acd530652954f65f5e97ebf59d50ffe6ccc76f4b
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-01-10 14:59:34 +0100

    Added scc styles to the user-list.component, buttons Edit and Delete to the user-list.component.html, import MatButtonModule, 'actions' to displayedColumns.

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -2,17 +2,18 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatTableModule} from '@angular/material/table'
 import { User } from '../../models/user.type'
 import { UserService } from '../../services/user-service/user.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-users-list',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatButtonModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent implements OnInit {
   userService = inject(UserService);
 
-  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'phoneNumber']
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'phoneNumber', 'actions']
   users = signal<Array<User>>([]);
 
   ngOnInit(): void {

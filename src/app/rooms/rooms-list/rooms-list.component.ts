021367bf1e6aae77ade35f0322269291354caf5c
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-01-12 21:21:30 +0100

    Created pagination for rooms.

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,17 +1,18 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { SnackbarService } from '../../services/snackbar/snackbar.service';
 import { MatDialog } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { RoomService } from '../../services/room-service/room.service';
 import { Room } from '../../models/room/room.type';
 import { RoomsEditComponent } from '../rooms-edit/rooms-edit.component';
 import { ConfirmDialogComponent } from '../../confirm-dialog/confirm-dialog.component';
 import { RoomsCreateComponent } from '../rooms-create/rooms-create.component';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-rooms-list',
-  imports: [MatTableModule, MatButtonModule],
+  imports: [MatTableModule, MatButtonModule, MatPaginatorModule],
   templateUrl: './rooms-list.component.html',
   styleUrl: './rooms-list.component.css'
 })
@@ -21,7 +22,12 @@ export class RoomsListComponent implements OnInit {
   dialog = inject(MatDialog);
 
   displayedColumns: string[] = ['id', 'roomNumber', 'roomType', 'pricePerNight', 'isAvailable', 'actions'];
-  rooms = signal<Array<Room>>([]);
+  dataSource = new MatTableDataSource<Room>();
+  pagedRooms = new MatTableDataSource<Room>();
+  totalRooms: number = 0;
+  pageSize: number = 5;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit(): void {
     this.loadRooms();
@@ -38,8 +44,12 @@ export class RoomsListComponent implements OnInit {
   }
 
   loadRooms(): void {
-    this.roomService.getRooms().subscribe((data: Array<Room>) =>
-      {this.rooms.set(data)});
+    this.roomService.getRooms().subscribe((rooms: Room[]) => {
+      this.dataSource.data = rooms;
+      this.totalRooms = rooms.length;
+      this.paginateRooms();
+      this.dataSource.paginator = this.paginator;
+    });
   }
 
   onEditRoom(room: Room): void {
@@ -77,5 +87,16 @@ export class RoomsListComponent implements OnInit {
       }
     });
   }
-  
+
+  paginateRooms(event?: PageEvent): void {
+    const pageIndex = event ? event.pageIndex : 0;
+    const pageSize = event ? event.pageSize : this.pageSize;
+    const startIndex = pageIndex * pageSize;
+    const endIndex = startIndex + pageSize;
+    this.pagedRooms.data = this.dataSource.data.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.paginateRooms(event);
+  }
 }
